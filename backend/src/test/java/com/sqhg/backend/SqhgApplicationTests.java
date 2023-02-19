679015ee0f67d1013e6d75fc7189a215ff3f3499
dniid <dniidtf2@gmail.com>
2023-02-19 17:45:39 -0300

    initial commit

---
+++ backend/src/test/java/com/sqhg/backend/SqhgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sqhg.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqhgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
