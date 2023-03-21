f34b0c8291468cda7653e60aeb48336cfe442596
Fernando Kei-iti Sakai Sano <fernando.sano@ssys.com.br>
2023-03-21 17:33:16 -0300

    feat: dummy routes for endpoints

--- sqhg/admin/router.py
+++ sqhg/admin/router.py
@@ -1,1 +1,11 @@
 """Admin's FastAPI router endpoints for SQHG's backend."""
+
+from fastapi import APIRouter
+
+
+router = APIRouter()
+
+
+@router.get('/')
+async def dummy_endpoint():
+    return {'message': 'admin'}
