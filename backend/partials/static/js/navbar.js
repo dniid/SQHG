b4d59653449020f588b95b79be0d4eb319e3b923
Mayhm <nisekoimylove@gmail.com>
2023-03-23 10:23:55 -0300

    feat!: desktop and mobile navbar
    BREAKING CHANGE: montado modelo padrão que deverá ser usado para outras telas

--- backend/partials/static/js/navbar.js
+++ backend/partials/static/js/navbar.js
@@ -11,6 +11,15 @@ const mobileNavClick = document.getElementById("mobile-nav-click");
 const mobileMenu = document.getElementById("mobile-menu");
 const content = document.getElementById("content");
 
+const mobileNavClick1 = document.getElementById("mobile-nav-click-1");
+const mobileNavClick2 = document.getElementById("mobile-nav-click-2");
+const mobileNavSubItens1 = document.getElementById("mobile-nav-sub-items-1");
+const mobileNavSubItens2 = document.getElementById("mobile-nav-sub-items-2");
+const mobileNavChevD1 = document.getElementById("mobile-nav-chevron-down-1");
+const mobileNavChevD2 = document.getElementById("mobile-nav-chevron-down-2");
+const mobileNavChevU1 = document.getElementById("mobile-nav-chevron-up-1");
+const mobileNavChevU2 = document.getElementById("mobile-nav-chevron-up-2");
+
 
 navClick1.addEventListener("click", (event) => {
     if (navSubItens1.classList.contains("hidden")) {
@@ -43,5 +52,29 @@ mobileNavClick.addEventListener("click", (event) => {
     } else {
         mobileMenu.classList.add("hidden");
         content.classList.remove("hidden");
+    }
+});
+
+mobileNavClick1.addEventListener("click", (event) => {
+    if (mobileNavSubItens1.classList.contains("hidden")) {
+        mobileNavSubItens1.classList.remove("hidden");
+        mobileNavChevU1.classList.remove("hidden");
+        mobileNavChevD1.classList.add("hidden");
+    } else {
+        mobileNavSubItens1.classList.add("hidden");
+        mobileNavChevU1.classList.add("hidden");
+        mobileNavChevD1.classList.remove("hidden");
+    }
+});
+
+mobileNavClick2.addEventListener("click", (event) => {
+    if (mobileNavSubItens2.classList.contains("hidden")) {
+        mobileNavSubItens2.classList.remove("hidden");
+        mobileNavChevU2.classList.remove("hidden");
+        mobileNavChevD2.classList.add("hidden");
+    } else {
+        mobileNavSubItens2.classList.add("hidden");
+        mobileNavChevU2.classList.add("hidden");
+        mobileNavChevD2.classList.remove("hidden");
     }
 });
