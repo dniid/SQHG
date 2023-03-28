9c999608c47ea245b3b99d3fe9b703a6f3886fea
Fernando Kei-iti Sakai Sano <fernando.sano@ssys.com.br>
2023-03-28 16:56:53 -0300

    fix: changed template to a dependency

--- sqhg/main.py
+++ sqhg/main.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 """Main FastAPI app for SQHG's backend."""
 
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Depends
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
 from core.database import BaseModel, engine
-from core.extensions import StaticFilesExtension
+from core.template import Template
 
 from utils.settings import find_dirs
 
@@ -25,16 +25,12 @@ app.include_router(survey.router.router, prefix='/survey', tags=['Survey'])
 app.include_router(user.router.router, prefix='/user', tags=['User'])
 
 
-static_dirs = find_dirs('.', 'static')
-for static in static_dirs:
+# Searches for directories named 'static' and then mount them dynamically
+for static in find_dirs('.', 'static'):
     app.mount(static[1:], StaticFiles(directory=static), name=static)
-
-templates = Jinja2Templates(directory=find_dirs('.', 'templates'))
-templates.env.static_dirs = static_dirs
-templates.env.add_extension(StaticFilesExtension)
 
 
 @app.get('/', response_class=HTMLResponse)
-async def homepage(request: Request):
+async def homepage(request: Request, templates: Jinja2Templates = Depends(Template)):
     context = {'request': request}
     return templates.TemplateResponse('components/navbar.html', context)
