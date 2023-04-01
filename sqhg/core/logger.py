28257a70dcc0d99bf10aff14d4bc3156077b16ac
dniid <dniidtf2@gmail.com>
2023-03-31 22:01:43 -0300

    fix: logging debug

--- sqhg/core/logger.py
+++ sqhg/core/logger.py
@@ -6,28 +6,27 @@ from pydantic import BaseModel
 class LogConfig(BaseModel):
     """Logging configuration to be set for the server"""
 
-    LOGGER_NAME: str = "sqhg"
-    LOG_FORMAT: str = "[%(asctime)s] [%(levelname)s] %(message)s"
-    LOG_LEVEL: str = "INFO"
+    LOGGER_NAME: str = 'sqhg'
+    LOG_FORMAT: str = '%(asctime)s | %(levelprefix)s | %(message)s'
+    LOG_LEVEL: str = 'DEBUG'
 
     # Logging config
     version = 1
     disable_existing_loggers = False
     formatters = {
-        "default": {
-            "()": "uvicorn.logging.DefaultFormatter",
-            "fmt": LOG_FORMAT,
-            "datefmt": "%Y-%m-%d %H:%M:%S",
+        'default': {
+            '()': 'uvicorn.logging.DefaultFormatter',
+            'fmt': LOG_FORMAT,
+            'datefmt': '%Y-%m-%d %H:%M:%S',
         },
     }
     handlers = {
-        "file": {
-            "formatter": "default",
-            "class": "logging.handlers.RotatingFileHandler",
-            "filename": "/var/log/sqhg/sqhg.log",
-            "level": "INFO",
+        'default': {
+            'formatter': 'default',
+            'class': 'logging.StreamHandler',
+            'stream': 'ext://sys.stderr',
         },
     }
     loggers = {
-        LOGGER_NAME: {"handlers": ["file"], "level": LOG_LEVEL},
+        LOGGER_NAME: {'handlers': ['default'], 'level': LOG_LEVEL},
     }
