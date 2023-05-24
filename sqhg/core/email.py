f5d77e019580f4dcb3524d472a83dff74a49621c
Fernando Kei-iti Sakai Sano <fernando.sano@ssys.com.br>
2023-05-24 11:22:50 -0300

    feat: email logging

--- sqhg/core/email.py
+++ sqhg/core/email.py
@@ -1,6 +1,8 @@
 """FastAPI Mail configuration for SQHG's backend."""
 
-from fastapi_mail import FastMail, ConnectionConfig
+import logging
+
+from fastapi_mail import FastMail, ConnectionConfig, MessageSchema
 
 from utils.settings import find_dirs
 from core.settings import (
@@ -13,18 +15,36 @@ from core.settings import (
 )
 
 
-async def Email():  # noqa: N802
-    config = ConnectionConfig(
-        MAIL_USERNAME=SMTP_USERNAME,
-        MAIL_PASSWORD=SMTP_PASSWORD,
-        MAIL_SERVER=SMTP_HOST,
-        MAIL_PORT=SMTP_PORT,
-        MAIL_FROM=SMTP_MAIL_FROM,
-        MAIL_STARTTLS=True,
-        MAIL_SSL_TLS=False,
-        TEMPLATE_FOLDER=find_dirs('auth', 'email')[0],
-        SUPPRESS_SEND=1 if ENVIRONMENT == 'development' else 0,
-        MAIL_DEBUG=1 if ENVIRONMENT == 'development' else 0,
-    )
+logger = logging.getLogger('sqhg')
 
-    yield FastMail(config)
+
+class FastMailConfig(FastMail):
+    """FastAPI Mail configuration for SQHG's backend."""
+
+    def __init__(self) -> None:
+        self.config = ConnectionConfig(
+            MAIL_USERNAME=SMTP_USERNAME,
+            MAIL_PASSWORD=SMTP_PASSWORD,
+            MAIL_SERVER=SMTP_HOST,
+            MAIL_PORT=SMTP_PORT,
+            MAIL_FROM=SMTP_MAIL_FROM,
+            MAIL_STARTTLS=True,
+            MAIL_SSL_TLS=False,
+            TEMPLATE_FOLDER=find_dirs('auth', 'email')[0],
+            SUPPRESS_SEND=1 if ENVIRONMENT == 'development' else 0,
+            MAIL_DEBUG=1 if ENVIRONMENT == 'development' else 0,
+        )
+
+    async def send_message(
+        self, message: MessageSchema, template_name: str = None
+    ) -> None:
+        if ENVIRONMENT == 'development':
+            with self.record_messages() as outbox:
+                await super().send_message(message, template_name)
+                logger.debug(outbox[0])
+        else:
+            await super().send_message(message, template_name)
+
+
+async def Email():  # noqa: N802
+    yield FastMailConfig()
