406356c99f97986da1918493cb5700e7e9747810
dniid <dniidtf2@gmail.com>
2023-06-29 21:21:29 -0300

    refactor: new tables structure migration

--- sqhg/survey/models.py
+++ sqhg/survey/models.py
@@ -36,18 +36,8 @@ class Survey(BaseModel):
 
     superior = relationship('Superior', back_populates='surveys')
     survey_model = relationship('SurveyModel', back_populates='surveys')
+    questions = relationship('Question', back_populates='survey')
     tokens = relationship('Token', back_populates='survey')
-
-
-class SurveyModel(BaseModel):
-    __tablename__ = 'survey_model'
-
-    id = Column(Integer, primary_key=True, index=True)
-    name = Column(String(255), nullable=False)
-    description = Column(String(255), nullable=False)
-
-    surveys = relationship('Survey', back_populates='survey_model')
-    questions = relationship('Question', back_populates='survey_model')
 
 
 class Question(BaseModel):
@@ -56,9 +46,9 @@ class Question(BaseModel):
     id = Column(Integer, primary_key=True, index=True)
     description = Column(String(255), nullable=False)
     type = Column(Enum(QuestionType), nullable=False)
-    survey_model_id = Column(Integer, ForeignKey('survey_model.id'), nullable=False, index=True)
+    survey_id = Column(Integer, ForeignKey('survey.id'), nullable=False, index=True)
 
-    survey_model = relationship('SurveyModel', back_populates='questions')
+    survey = relationship('Survey', back_populates='questions')
     options = relationship('Option', back_populates='question')
     answers = relationship('Answer', back_populates='question')
 
@@ -73,12 +63,44 @@ class Option(BaseModel):
     question = relationship('Question', back_populates='options')
 
 
+class SurveyModel(BaseModel):
+    __tablename__ = 'survey_model'
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(255), nullable=False)
+    description = Column(String(255), nullable=False)
+
+    surveys = relationship('Survey', back_populates='survey_model')
+    questions_model = relationship('QuestionModel', back_populates='survey_model')
+
+
+class QuestionModel(BaseModel):
+    __tablename__ = 'question_model'
+
+    id = Column(Integer, primary_key=True, index=True)
+    description = Column(String(255), nullable=False)
+    type = Column(Enum(QuestionType), nullable=False)
+    survey_model_id = Column(Integer, ForeignKey('survey_model.id'), nullable=False, index=True)
+
+    survey_model = relationship('SurveyModel', back_populates='questions_model')
+    options_model = relationship('OptionModel', back_populates='question_model')
+
+
+class OptionModel(BaseModel):
+    __tablename__ = 'option_model'
+
+    id = Column(Integer, primary_key=True, index=True)
+    description = Column(String(255), nullable=False)
+    question_model_id = Column(Integer, ForeignKey('question_model.id'), nullable=False, index=True)
+
+    question_model = relationship('QuestionModel', back_populates='options_model')
+
+
 class Answer(BaseModel):
     __tablename__ = 'answer'
 
     id = Column(Integer, primary_key=True, index=True)
     answer = Column(String(255), nullable=False)
     question_id = Column(Integer, ForeignKey('question.id'), nullable=False, index=True)
-    survey_id = Column(Integer, ForeignKey('survey.id'), nullable=False, index=True)
 
     question = relationship('Question', back_populates='answers')
